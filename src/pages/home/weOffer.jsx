a0832d692ec46faaa29f5269433528b095e0e400
Abraham-ch <abraham.chafloque.a@uni.pe>
2025-01-04 00:38:02 -0500

    feat: offerCardReverse

--- src/pages/home/weOffer.jsx
+++ src/pages/home/weOffer.jsx
@@ -1,4 +1,4 @@
-import { OfferCard } from "src/components/ui/offerCard";
+import { OfferCard, OfferCardReverse } from "src/components/ui/offerCard";
 import card1 from "assets/svg/offerCard1.svg"
 import card2 from "assets/svg/offerCard2.svg"
 import card3 from "assets/svg/offerCard3.svg"
@@ -8,11 +8,11 @@ export const WeOffer = () => {
     <section className="py-20">
       <h2 className="text-3xl font-bold pb-16">¿Cómo funciona?</h2>
       <div className="flex flex-col gap-y-20 items-center">
-        <OfferCard title="Prepárate con nosotros" description="Obtendrás una preparación completa para ingresar a la universidad. Enfócate en las áreas donde más lo necesitas." img={card1} alt="Offer card" cn={"-reverse"} />
+        <OfferCard title="Prepárate con nosotros" description="Obtendrás una preparación completa para ingresar a la universidad. Enfócate en las áreas donde más lo necesitas." img={card1} alt="Offer card" />
 
-        <OfferCard title="Participa en simulacros" description="Practica con exámenes reales en un ambiente controlado. Analizamos tus resultados y te ayudamos a mejorar tu rendimiento." img={card2} alt="Offer card" cn={""} />
+        <OfferCardReverse title="Participa en simulacros" description="Practica con exámenes reales en un ambiente controlado. Analizamos tus resultados y te ayudamos a mejorar tu rendimiento." img={card2} alt="Offer card" />
 
-        <OfferCard title="Conéctate con tu meta" description="Recibe orientación vocacional y elige la carrera ideal para ti. Nuestro equipo te acompaña en cada paso del camino." img={card3} alt="Offer card" cn={"-reverse"} />
+        <OfferCard title="Conéctate con tu meta" description="Recibe orientación vocacional y elige la carrera ideal para ti. Nuestro equipo te acompaña en cada paso del camino." img={card3} alt="Offer card" />
       </div>
     </section>
     
